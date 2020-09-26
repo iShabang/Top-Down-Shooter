1484aa6875c531ab13da93402247ea733dda23c2
iShaBaNg <montos2@unlv.nevada.edu>
2020-09-26 13:34:16 -0700

    added script for mouse rotation along the x axis

---
+++ Assets/Scripts/MouseRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+    [SerializeField] private float mouseX = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up * Time.deltaTime * mouseX);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float move_speed = 10.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
