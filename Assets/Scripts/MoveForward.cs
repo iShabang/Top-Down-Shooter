d4a21845f6444ac71353cac9f3fc76a458a7c087
iShaBaNg <montos2@unlv.nevada.edu>
2020-09-26 14:23:19 -0700

    added script to move bullets and objects in a designated direction

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float x = 0;
+    [SerializeField] private float y = 0;
+    [SerializeField] private float z = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(x,y,z) * Time.deltaTime * speed);
+    }
+}
