21a81b18f77d0cbff0cc0647f7f97bedda28c7e5
iShaBaNg <montos2@unlv.nevada.edu>
2020-09-27 16:08:45 -0700

    Character now looks at the mouse position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 10.0f;
     [SerializeField] private GameObject[] weapons;
 
-    private GameObject rotationLayer;
-    
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        rotationLayer = transform.Find("RotationLayer").gameObject;
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
@@ -22,5 +22,16 @@ public class PlayerController : MonoBehaviour
         float vInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(hInput,0, vInput);
         transform.Translate(direction * Time.deltaTime * moveSpeed);
+
+        // Mouse Aim
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up,Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(pointToLook.x,transform.position.y,pointToLook.z));
+        }
     }
 }
