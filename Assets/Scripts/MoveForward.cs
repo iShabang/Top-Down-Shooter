3db2b9ab692e13b712c08458556f3133592e4ad4
iShaBaNg <montos2@unlv.nevada.edu>
2020-10-01 19:48:52 -0700

    Simplified move script

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     [SerializeField] private float speed = 10.0f;
-    [SerializeField] private float x = 0;
-    [SerializeField] private float y = 0;
-    [SerializeField] private float z = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +15,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(x,y,z) * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
