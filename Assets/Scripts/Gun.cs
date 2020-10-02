b8bd2214ef4e77a9fb397a2fb874a23b06940923
iShaBaNg <montos2@unlv.nevada.edu>
2020-10-01 19:49:31 -0700

    Added firing point to Gun prefab and script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour, iWeapon
 {
+    [SerializeField] private GameObject bulletPrefab = null;
+    [SerializeField] private Transform firePoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void fire()
     {
-        Debug.Log("Im a gun!!!");
+        GameObject bullet = Instantiate(bulletPrefab,firePoint.position,firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.forward * 10, ForceMode.Impulse);
     }
 }
