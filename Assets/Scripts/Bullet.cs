830430264c5a1355a0a73524570983def9fcabf3
iShaBaNg <montos2@unlv.nevada.edu>
2020-10-04 18:46:54 -0700

    Added particle animation for basic bullets.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    [SerializeField] private GameObject pfParticle;
     void OnCollisionEnter(Collision other)
     {
+        GameObject particles = Instantiate(pfParticle, transform.position, Quaternion.identity); 
         Destroy(gameObject);
+        Destroy(particles, 1f);
     }
 }
